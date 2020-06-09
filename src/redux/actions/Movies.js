6eaf0f2c7858ef0816ff794cb1ff9ca2e9c0e71a
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-06-09 17:33:49 +0530

    [BUILD] Home

--- src/redux/actions/Movies.js
+++ src/redux/actions/Movies.js
@@ -4,11 +4,11 @@ import {axiosNoAuth} from 'config/axios-instances';
 export function getMoviesDetails(successCallBack,errorCallBack){
     return async function(dispatch){
         try{
-            let responseGoW = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=Gangs+of+Wasseypur`);
+            let responseGoW = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=Barfi!`);
             let responsequeen = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=Queen`);
             let responseudaan = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=Udaan`);
             let responsekahaani = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=Kahaani`);
-            let responsebmb = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=Bhaag+Milkha+Bhaag`);
+            let responsebmb = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=Dev.D `);
             let responseanand = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=anand`);
             let responseaw = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=A+Wednesday`);
             let responsehf = await axiosNoAuth.get(`http://www.omdbapi.com/?i=tt3896198&apikey=dafa6536&t=Hera+Pheri`);
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
+import MoviePoster from 'views/components/MoviePoster';
+import {connect} from 'react-redux';
+import {getMoviesDetails} from 'redux/actions/Movies';
+import {getPopularTvSeriesInfo,getDefaultTvSeriesInfo} from 'redux/actions/TvSeries';
 
 class Home extends Component {
+
+    componentDidMount = () => {
+        this.props.getMoviesDetails();
+        this.props.getPopularTvSeriesInfo();
+        this.props.getDefaultTvSeriesInfo();
+    }
+
     render() {
         return (
-            <div>
-                Home
+            <div className="parent-row parent-wrap">
+                {this.props.default_tv_series.length && this.props.default_tv_series.map((value,index)=>
+                    <div className="mb-20 mr-20 movie-card" key={index}>
+                        <MoviePoster 
+                            poster={value.Poster}
+                            title={value.Title}
+                            imdbRating={value.imdbRating}
+                        />
+                    </div>
+                )}
+                {this.props.popular_tv_series.length && this.props.popular_tv_series.map((value,index)=>
+                    <div className="mb-20 mr-20 movie-card" key={index}>
+                        <MoviePoster 
+                            poster={value.Poster}
+                            title={value.Title}
+                            imdbRating={value.imdbRating}
+                        />
+                    </div>
+                )}
+                {this.props.movies_data.length && this.props.movies_data.map((value,index)=>
+                    <div className="mb-20 mr-20 movie-card" key={index}>
+                        <MoviePoster 
+                            poster={value.Poster}
+                            title={value.Title}
+                            imdbRating={value.imdbRating}
+                        />
+                    </div>
+                )}
             </div>
         );
     }
 }
 
-export default Home;
+// export default Home;
+function mapStateToProps(state){
+    return{
+        movies_data:state.Movies.movies_data,
+        popular_tv_series:state.TvSeries.popular_tv_series,
+        default_tv_series:state.TvSeries.default_tv_series
+    }
+}
+
+export default connect(mapStateToProps,{
+    getMoviesDetails,
+    getPopularTvSeriesInfo,
+    getDefaultTvSeriesInfo
+})(Home);
