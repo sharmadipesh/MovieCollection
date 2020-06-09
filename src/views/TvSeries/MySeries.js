366ba8894b0af831b1d3e4943d1f373549a211af
Dipesh Sharma <dipesh_sharma@gridle.io>
2020-06-09 13:50:30 +0530

    [BUILD] Active Deactive Card

--- src/views/TvSeries/MySeries.js
+++ src/views/TvSeries/MySeries.js
@@ -7,18 +7,20 @@ import idx from 'idx';
 class MySeries extends Component {
 
     state = {
+        activeCardIndex:0,
         activeCardDetails:{}
     }
 
-    activeSeriesHandler = value => {
+    activeSeriesHandler = (value,index) => {
         this.setState({
-            activeCardDetails:value
+            activeCardDetails:value,
+            activeCardIndex:index
         })
     }
 
     componentDidMount = async() =>{
         await this.props.getDefaultTvSeriesInfo();
-        this.activeSeriesHandler(idx(this.props.default_tv_series,_=>_[0]));
+        this.activeSeriesHandler(idx(this.props.default_tv_series,_=>_[0]),0);
     }
 
     render() {
@@ -41,15 +43,53 @@ class MySeries extends Component {
                         <img alt="poster" className="active-poster-image" src="img/narcos.jpg"/>
                     </div>
                 </div> */}
-                <div className="parent-row parent-wrap parent-v-center mb-30">
+                {/* <div className="parent-row parent-wrap parent-v-center mb-30">
                     {this.props.default_tv_series.length && this.props.default_tv_series.map((value,index)=>
-                        <div className="movie-card mr-20 clickable" key={index} onClick={()=>this.activeSeriesHandler(value)}>
+                        <div 
+                            className="movie-card mr-20 clickable" 
+                            key={index} 
+                            onClick={()=>this.activeSeriesHandler(value)}
+                            >
                             <img alt="movie-poster" src={value.Poster} className="movie-poster mb-7"/>
                             <div className="movie-name">{value.Title}</div>
                             <div className="movie-rating">iMDB Rating: {value.imdbRating}/10</div>
                         </div>
                     )}
+                </div> */}
+                <div className="parent-row parent-wrap parent-v-center mb-50">
+                    {this.props.default_tv_series.length && this.props.default_tv_series.map((value,index)=>
+                        <div 
+                            className={this.state.activeCardIndex === index ? 'active-card mr-20 ' : 'movie-card mr-20 clickable'}
+                            key={index} 
+                            onClick={()=>this.activeSeriesHandler(value,index)}
+                            >
+                            {this.state.activeCardIndex !== index ?
+                                <div>
+                                    <img alt="movie-poster" src={value.Poster} className="movie-poster mb-7"/>
+                                    <div className="movie-name">{value.Title}</div>
+                                    <div className="movie-rating">iMDB Rating: {value.imdbRating}/10</div>
+                                </div>
+                                :
+                                <div className="parent-row parent-h-space-between">
+                                    <div className="parent-col parent-h-space-between">
+                                        <div>
+                                            <div className="series-name">{value.Title}</div>
+                                            <div>{value.totalSeasons} Seasons</div>
+                                            <div>{value.imdbVotes} iMDB Votes</div>
+                                        </div>
+                                        <div>
+                                            <div>iMdB Rating: {value.imdbRating}/10</div>
+                                            <div>Go to iMDB Page</div>
+                                        </div>
+                                    </div>
+                                    <img alt="poster" className="active-poster-image" src={value.Poster}/>
+                                </div>
+                            }
+                        </div>
+                    )}
                 </div>
+
+
                 <div className="parent-row parent-wrap parent-v-center">
                     {Object.keys(this.state.activeCardDetails).length && 
                         Array.from(Array(Number(this.state.activeCardDetails.totalSeasons)), (e, i) => {
